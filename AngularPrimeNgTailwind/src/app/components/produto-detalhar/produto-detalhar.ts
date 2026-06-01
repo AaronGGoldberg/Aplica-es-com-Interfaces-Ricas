3c53a755727703c59b3d2a97c0dfc8c6a79175f2
Aaron Goldberg <aaron.guerra137@gmail.com>
2026-06-01 05:37:54 +0000

    refactor(produto-detalhar): lê produto enviado pela rota

--- AngularPrimeNgTailwind/src/app/components/produto-detalhar/produto-detalhar.ts
+++ AngularPrimeNgTailwind/src/app/components/produto-detalhar/produto-detalhar.ts
@@ -1,14 +1,46 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 
+import { Produto } from '../../models/produto';
 import { ProdutoService } from '../../services/produto';
 
 @Component({
   selector: 'app-produto-detalhar',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, RouterLink],
   templateUrl: './produto-detalhar.html'
 })
-export class ProdutoDetalharComponent {
+export class ProdutoDetalharComponent implements OnInit {
   readonly produtoService = inject(ProdutoService);
+  readonly produtoEncontrado = signal(true);
+
+  private readonly activatedRoute = inject(ActivatedRoute);
+  private readonly router = inject(Router);
+
+  ngOnInit() {
+    const produtoDaRota = this.produtoRecebidoPelaRota();
+    const produtoId = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+
+    if (produtoDaRota?.id === produtoId) {
+      this.produtoService.definirProdutoDetalhado(produtoDaRota);
+      this.produtoEncontrado.set(true);
+      return;
+    }
+
+    const produto = this.produtoService.detalhar(produtoId);
+    this.produtoEncontrado.set(Boolean(produto));
+  }
+
+  fechar() {
+    this.produtoService.fecharDetalhes();
+    this.router.navigate(['/produtos']);
+  }
+
+  private produtoRecebidoPelaRota(): Produto | null {
+    const state = this.router.getCurrentNavigation()?.extras.state ?? history.state;
+    const produto = state?.['produto'] as Produto | undefined;
+
+    return produto ?? null;
+  }
 }
