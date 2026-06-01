ec3e95e858f81e20c045bda4b4269a7b21608e36
Aaron Goldberg <aaron.guerra137@gmail.com>
2026-06-01 05:40:15 +0000

    refactor(app): usa router outlet e navegação principal

--- AngularPrimeNgTailwind/src/app/app.ts
+++ AngularPrimeNgTailwind/src/app/app.ts
@@ -1,23 +1,14 @@
 import { Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
 
-import { ProdutoAlterarComponent } from './components/produto-alterar/produto-alterar';
-import { ProdutoDetalharComponent } from './components/produto-detalhar/produto-detalhar';
-import { ProdutoIncluirComponent } from './components/produto-incluir/produto-incluir';
-import { ProdutoListarComponent } from './components/produto-listar/produto-listar';
 import { ProdutoService } from './services/produto';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    ToastModule,
-    ProdutoAlterarComponent,
-    ProdutoDetalharComponent,
-    ProdutoIncluirComponent,
-    ProdutoListarComponent
-  ],
+  imports: [RouterOutlet, ToastModule],
   providers: [MessageService],
   templateUrl: './app.html'
 })
