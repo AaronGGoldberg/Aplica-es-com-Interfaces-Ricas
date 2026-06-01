e710cd512e8cf70548e7a530b0c3e2f6be275edd
Aaron Goldberg <aaron.guerra137@gmail.com>
2026-06-01 04:43:56 +0000

    refactor(produto-listar): usa service para listar produtos

--- AngularPrimeNgTailwind/src/app/components/produto-listar/produto-listar.ts
+++ AngularPrimeNgTailwind/src/app/components/produto-listar/produto-listar.ts
@@ -1,19 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { Component, input, output } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 
-import { Produto } from '../../models/produto';
+import { ProdutoRemoverComponent } from '../produto-remover/produto-remover';
+import { ProdutoService } from '../../services/produto';
 
 @Component({
   selector: 'app-produto-listar',
   standalone: true,
-  imports: [ButtonModule, CommonModule, TableModule],
+  imports: [ButtonModule, CommonModule, TableModule, ProdutoRemoverComponent],
   templateUrl: './produto-listar.html'
 })
 export class ProdutoListarComponent {
-  readonly produtos = input.required<Produto[]>();
-  readonly editar = output<Produto>();
-  readonly remover = output<Produto>();
-  readonly detalhar = output<Produto>();
+  readonly produtoService = inject(ProdutoService);
+
+  editar(id: number) {
+    this.produtoService.selecionarParaEdicao(id);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  detalhar(id: number) {
+    this.produtoService.detalhar(id);
+  }
 }
