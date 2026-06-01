818a721579740b53475d517d6433b1060b0dc1f1
Aaron Goldberg <aaron.guerra137@gmail.com>
2026-06-01 05:39:47 +0000

    feat(routes): cria rotas do CRUD de produtos

--- AngularPrimeNgTailwind/src/app/app.routes.ts
+++ AngularPrimeNgTailwind/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ProdutoAlterarComponent } from './components/produto-alterar/produto-alterar';
+import { ProdutoDetalharComponent } from './components/produto-detalhar/produto-detalhar';
+import { ProdutoIncluirComponent } from './components/produto-incluir/produto-incluir';
+import { ProdutoListarComponent } from './components/produto-listar/produto-listar';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'produtos',
+    pathMatch: 'full'
+  },
+  {
+    path: 'produtos',
+    component: ProdutoListarComponent
+  },
+  {
+    path: 'produtos/novo',
+    component: ProdutoIncluirComponent
+  },
+  {
+    path: 'produtos/:id/detalhar',
+    component: ProdutoDetalharComponent
+  },
+  {
+    path: 'produtos/:id/editar',
+    component: ProdutoAlterarComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'produtos'
+  }
+];
